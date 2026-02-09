592502d18dbf24017213076080e2a606579d5185
jmot4 <jmota@gtech-services.com>
2026-02-09 13:52:43 -0600

    Se agregan 3 estilos de botones

--- app/src/main/java/com/atom/firstappcompose/MainActivity.kt
+++ app/src/main/java/com/atom/firstappcompose/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.atom.firstappcompose.ui.theme.FirstAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,12 +30,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstAppComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Colum(
+                    Column(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .fillMaxSize()
-                    ){
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
                         SimpleButton()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        ButtonWithColor()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        ButtonWithTwoColors()
                     }
                 }
             }
@@ -35,9 +51,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SimpleButton(){
+fun SimpleButton() {
     Button(onClick = { /*TODO*/ }) {
         Text(text = "Ejemplo Boton")
+    }
+}
+
+@Composable
+fun ButtonWithColor() {
+    Button(
+        onClick = {
+            //your onclick code
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+    )
+    {
+        Text(text = "Button with gray background", color = Color.White)
+    }
+}
+
+@Composable
+fun ButtonWithTwoColors(){
+    Button(onClick = { /*TODO*/ },
+        colors=ButtonDefaults.buttonColors(containerColor = Color.LightGray)){
+        Text(text="Mitad y",color=Color.Magenta)
+        Text(text="mitad",color= Color.Green)
     }
 }
 
