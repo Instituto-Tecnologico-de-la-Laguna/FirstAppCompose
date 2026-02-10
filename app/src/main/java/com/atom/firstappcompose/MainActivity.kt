951ec14de3d16592508bed1713d655847019a5dd
jmot4 <jmota@gtech-services.com>
2026-02-10 13:53:01 -0600

    Se agregan boton con Icono

--- app/src/main/java/com/atom/firstappcompose/MainActivity.kt
+++ app/src/main/java/com/atom/firstappcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
@@ -18,7 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atom.firstappcompose.ui.theme.FirstAppComposeTheme
@@ -43,6 +47,8 @@ class MainActivity : ComponentActivity() {
                         ButtonWithColor()
                         Spacer(modifier = Modifier.height(16.dp))
                         ButtonWithTwoColors()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        ButtonWithIcon()
                     }
                 }
             }
@@ -76,6 +82,19 @@ fun ButtonWithTwoColors(){
         colors=ButtonDefaults.buttonColors(containerColor = Color.LightGray)){
         Text(text="Mitad y",color=Color.Magenta)
         Text(text="mitad",color= Color.Green)
+    }
+}
+
+@Composable
+fun ButtonWithIcon(){
+    Button(onClick = { /*TODO*/ }) {
+        Image(
+            painterResource(id=R.drawable.ic_launcher_foreground),
+            contentDescription = "Cart Icon",
+            colorFilter = ColorFilter.tint(Color.White),
+            modifier = Modifier.size(20.dp)
+        )
+        Text(text = "Add to Cart")
     }
 }
 
